009496a1e73ac68509df08ed8c4c30e91a357c81
Ali Tourani <a.tourani1991@gmail.com>
2023-07-28 16:05:46 +0200

    [feat] - improve mono setup

--- mainUsbMono.py
+++ mainUsbMono.py
@@ -1,17 +1,43 @@
 import cv2 as cv
-from config import ports, fpsBoost
+import PySimpleGUI as sg
+from src.gui.guiElements import guiElements
 import src.csr_sensors.sensors.sensorUSB as usb
+from src.csr_detector.process import processMonoFrame
+from config import ports, fpsBoost, windowWidth, windowLocation
 
 
 def main():
-    cap = usb.createCameraObject(ports['lCam'])
+    print('Framework started! [USB Camera Mono Setup]')
+
+    # Create the window
+    windowTitle, tabGroup, imageViewer = guiElements()
+    window = sg.Window(
+        windowTitle, [tabGroup, imageViewer], location=windowLocation)
+
+    cap = usb.createCameraObject(ports['rCam'])
 
     if fpsBoost:
         cap.set(cv.CAP_PROP_FPS, 30.0)
 
     while True:
+        event, values = window.read(timeout=10)
+
+        # End program if user closes window
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+
         # Retrieve frames
         ret, frame = usb.grabImage(cap)
+
+        # Get the values from the GUI
+        params = {'circlularMaskCoverage': values['CircMask'], 'threshold': values['Threshold'],
+                  'erosionKernel': values['Erosion'], 'gaussianKernel': values['Gaussian'],
+                  'enableCircularMask': values['CircMaskEnable'], 'allChannels': values['AChannels'],
+                  'rChannel': values['RChannel'], 'gChannel': values['GChannel'], 'bChannel': values['BChannel'],
+                  'threshboth': values['ThreshBoth'], 'threshbin': values['ThreshBin'],
+                  'threshots': values['ThreshOts'], 'isMarkerLeftHanded': values['MarkerLeftHanded'],
+                  'windowWidth': windowWidth
+                  }
 
         # Check if the frame is successfully read
         if not ret:
@@ -20,23 +46,18 @@ def main():
 
         # Change brightness
         frame = cv.convertScaleAbs(
-            frame, alpha=2.0, beta=0.0)
+            frame, alpha=values['camAlpha'], beta=values['camBeta'])
 
-        # Convert the image to grayscale
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-        # Thresholding
-        _, mask = cv.threshold(frame, 5, 255,
-                               cv.THRESH_BINARY)
+        # Process frames
+        frame, mask = processMonoFrame(frame, ret, params)
 
         # Show the frames
-        cv.imshow("USB Camera Feed", mask)
-
-        # Exit the loop if the 'q' key is pressed
-        if cv.waitKey(1) == ord('q'):
-            break
+        frame = cv.imencode(".png", frame)[1].tobytes()
+        window['Frames'].update(data=frame)
 
     cap.release()
+    window.close()
+    print('Framework finished! [USB Camera Mono Setup]')
 
 
 # Run the program
