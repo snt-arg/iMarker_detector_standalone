89d664c31665ae9c39130dfe7e935eb58beb82f4
Ali Tourani <a.tourani1991@gmail.com>
2024-08-07 12:03:22 +0200

    [feat] - improve offline image

--- src/runner_offImg.py
+++ src/runner_offImg.py
@@ -15,7 +15,8 @@ def runner_offImg(config):
     cfgOffline = config['sensor']['offline']
 
     setupVariant = "Sequential Subtraction" if cfgMode['sequentialSubtraction'] else "Masking"
-    print(f'Framework started! [Offline Image Setup - {setupVariant}]')
+    print(
+        f'Framework started! [Offline Images Captured by Single Vision Setup - {setupVariant}]')
 
     # Check if the images files exist
     image1Path = os.path.join(
@@ -25,6 +26,10 @@ def runner_offImg(config):
     if not os.path.exists(image1Path) or not os.path.exists(image2Path):
         print("At leaset one image does not exist!")
         return
+
+    # Variables
+    frameMask = None
+    frameMaskApplied = None
 
     # Create the window
     window = getGUI(config, True)
@@ -51,49 +56,52 @@ def runner_offImg(config):
             frame2Raw = cv.convertScaleAbs(
                 frame2Raw, alpha=values['camAlpha'], beta=values['camBeta'])
 
-            # Convert to HSV
-            colorFrame1 = cv.cvtColor(frame1Raw, cv.COLOR_BGR2HSV)
-            colorFrame2 = cv.cvtColor(frame2Raw, cv.COLOR_BGR2HSV)
-
             if (cfgMode['sequentialSubtraction']):
-                pFrame, cFrame, mask = processSequentialFrames(
-                    colorFrame1, colorFrame2, True, config)
+                # Process the frames
+                pFrame, cFrame, frameMask = processSequentialFrames(
+                    frame1Raw, frame2Raw, True, config)
                 # Apply the mask
-                frameMasked = cv.bitwise_and(pFrame, pFrame, mask=mask)
+                frameMaskApplied = cv.bitwise_and(
+                    cFrame, cFrame, mask=frameMask)
+                # Show the setup-specific frames
+                pFrameVis = cv.imencode(".png", frame1Raw)[1].tobytes()
+                cFrameVis = cv.imencode(".png", frame2Raw)[1].tobytes()
+                window['FramesLeft'].update(data=pFrameVis)
+                window['FramesRight'].update(data=cFrameVis)
             else:
-                frame, mask = processSingleFrame(colorFrame1, True, config)
+                # Process the frames
+                cFrame, frameMask = processSingleFrame(
+                    frame2Raw, True, config)
                 # Apply the mask
-                frameMasked = cv.bitwise_and(frame, frame, mask=mask)
+                frameMaskApplied = cv.bitwise_and(
+                    cFrame, cFrame, mask=frameMask)
+                # Show the setup-specific frames
+                cFrameVis = cv.imencode(".png", frame2Raw)[1].tobytes()
+                window['FramesMain'].update(data=cFrameVis)
 
-            # Show the frames
-            if (cfgMode['sequentialSubtraction']):
-                pFrame = cv.imencode(".png", pFrame)[1].tobytes()
-                cFrame = cv.imencode(".png", cFrame)[1].tobytes()
-                window['FramesLeft'].update(data=pFrame)
-                window['FramesRight'].update(data=cFrame)
-            else:
-                frame = cv.imencode(".png", frame1Raw)[1].tobytes()
-                window['FramesMain'].update(data=frame)
-            newMask = cv.imencode(".png", mask)[1].tobytes()
-            window['FramesMask'].update(data=newMask)
-            newFrameMasked = cv.imencode(".png", frameMasked)[1].tobytes()
-            window['FramesMaskApplied'].update(data=newFrameMasked)
+            # Show the common frames
+            maskVis = cv.imencode(".png", frameMask)[1].tobytes()
+            maskAppliedVis = cv.imencode(".png", frameMaskApplied)[1].tobytes()
+            window['FramesMask'].update(data=maskVis)
+            window['FramesMaskApplied'].update(data=maskAppliedVis)
 
             # ArUco marker detection
-            detectedMarkers = arucoMarkerDetector(
-                mask, cfgMarker['detection']['dictionary'])
-            newDetectedMarkers = cv.imencode(
-                ".png", detectedMarkers)[1].tobytes()
-            window['FramesMarker'].update(data=newDetectedMarkers)
+            frameMarkers = arucoMarkerDetector(
+                frameMask, cfgMarker['detection']['dictionary'])
+            frameMarkersVis = cv.imencode(
+                ".png", frameMarkers)[1].tobytes()
+            window['FramesMarker'].update(data=frameMarkersVis)
 
             # Record the frame(s)
             if event == 'Record':
-                concatedImage = imageConcatHorizontal(
-                    [frame1Raw, frame2Raw, frameMasked], 1200)
+                frameMarkers = cv.cvtColor(frameMarkers, cv.COLOR_GRAY2BGR)
+                imageList = [frame1Raw, frame2Raw, frameMarkers] if (
+                    cfgMode['sequentialSubtraction']) else [frame2Raw, frameMarkers]
+                concatedImage = imageConcatHorizontal(imageList, 1800)
                 frameSave(concatedImage, cfgMode['runner'])
 
     finally:
         # Stop the pipeline and close the windows
         cv.destroyAllWindows()
         print(
-            f'Framework stopped! [Offline Image Setup - {setupVariant}]')
+            f'Framework stopped! [Offline Images Captured by Single Vision Setup - {setupVariant}]')
