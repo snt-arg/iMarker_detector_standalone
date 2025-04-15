9f09f457018beea37145aca037759d0ac0ebbb89
Ali Tourani <a.tourani1991@gmail.com>
2025-04-15 15:53:07 +0200

    [feat] - take offline video type to new GUI

--- src/runner_offVid.py
+++ src/runner_offVid.py
@@ -1,11 +1,13 @@
 import os
 import cv2 as cv
 import numpy as np
+import dearpygui.dearpygui as dpg
 from .gui.utils import resizeFrame, frameSave
 from .csr_detector.vision.concatImages import imageConcatHorizontal
 from .marker_detector.arucoMarkerDetector import arucoMarkerDetector
-from .gui.guiElements import checkTerminateGUI, getGUI, updateColorPreview
 from .csr_detector.process import processSequentialFrames, processSingleFrame
+from .csr_sensors.sensors.config.cameraPresets import cameraMatrix_RealSense, distCoeffs_RealSense
+from .gui.guiContent import guiElements, loadImageAsTexture, onImageViewTabChange, updateImageTexture, updateWindowSize
 
 
 def runner_offVid(config):
@@ -14,6 +16,7 @@ def runner_offVid(config):
     cfgMode = config['mode']
     cfgMarker = config['marker']
     cfgOffline = config['sensor']['offline']
+    isSequential = cfgMode['sequentialSubtraction']
 
     setupVariant = "Sequential Subtraction" if cfgMode['sequentialSubtraction'] else "Masking"
     print(
@@ -25,12 +28,11 @@ def runner_offVid(config):
         return
 
     # Variables
+    cFrameRGB = None
     prevFrame = None
     frameMask = None
+    singleCamera = True
     frameMaskApplied = None
-
-    # Create the window
-    window = getGUI(config, True)
 
     # Open the video file
     cap = cv.VideoCapture(cfgOffline['video']['path'])
@@ -39,116 +41,171 @@ def runner_offVid(config):
         print("Error: Could not open video file.")
         exit()
 
-    try:
-        while True:
-            event, values = window.read(timeout=10)
+    # Read the first frame to get the size
+    width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+    initFrame = np.zeros((height, width, 3), dtype=np.uint8)
+    initFrame = resizeFrame(initFrame, cfgGui['maxImageHolderSize'])
+    height, width = initFrame.shape[:2]
 
-            # End program if user closes window
-            if checkTerminateGUI(event):
-                break
+    # Initialize the GUI
+    dpg.create_context()
+    dpg.create_viewport(title='iMarker Detector Software')
+    dpg.setup_dearpygui()
+    dpg.set_viewport_resize_callback(updateWindowSize)
 
-            # Retrieve frames
-            ret, currFrame = cap.read()
+    # Load logo image
+    loadImageAsTexture("./src/logo.png", "LogoImage")
 
-            # Break out of the loop if the video is finished
-            if not ret:
-                break
+    # Use an invisible container for internal values
+    with dpg.value_registry():
+        dpg.add_bool_value(default_value=False, tag="RecordFlag")
 
-            # Resize frames if necessary
-            currFrame = resizeFrame(currFrame, cfgGui['maxImageHolderSize'])
+    # Register a render callback (executed after GUI is ready)
+    postInitImages = [(initFrame, 'FramesMain'),
+                      (initFrame, 'FramesLeft'),
+                      (initFrame, 'FramesRight'),
+                      (initFrame, 'FramesMask'),
+                      (initFrame, 'FramesMaskApplied'),
+                      (initFrame, 'FramesMarker')
+                      ]
 
-            # Change brightness
-            currFrame = cv.convertScaleAbs(
-                currFrame, alpha=values['camAlpha'], beta=values['camBeta'])
+    def updateAfterGui():
+        for img, tag in postInitImages:
+            updateImageTexture(img, tag)
+    dpg.set_frame_callback(1, updateAfterGui)
 
-            # Rotate the frame 180 degrees (if necessary)
-            if cfgOffline['video']['rotate']:
-                currFrame = cv.rotate(currFrame, cv.ROTATE_180)
+    # Define textures
+    with dpg.texture_registry(show=True):
+        dpg.add_dynamic_texture(width, height, default_value=[
+                                0.0, 0.0, 0.0, 1.0]*width*height, tag="FramesMain")
+        dpg.add_dynamic_texture(width, height, default_value=[
+                                0.0, 0.0, 0.0, 1.0]*width*height, tag="FramesLeft")
+        dpg.add_dynamic_texture(width, height, default_value=[
+                                0.0, 0.0, 0.0, 1.0]*width*height, tag="FramesRight")
+        dpg.add_dynamic_texture(width, height, default_value=[
+                                0.0, 0.0, 0.0, 1.0]*width*height, tag="FramesMask")
+        dpg.add_dynamic_texture(width, height, default_value=[
+                                0.0, 0.0, 0.0, 1.0]*width*height, tag="FramesMaskApplied")
+        dpg.add_dynamic_texture(width, height, default_value=[
+                                0.0, 0.0, 0.0, 1.0]*width*height, tag="FramesMarker")
 
-            # Only the first time, copy the current frame to the previous frame
-            if prevFrame is None:
-                prevFrame = np.copy(currFrame)
+    # GUI content
+    guiElements(config, singleCamera)
 
-            # Check variable changes from the GUI
-            config['algorithm']['process']['subtractRL'] = values['SubtractionOrder']
-            config['algorithm']['postprocess']['erosionKernelSize'] = values['Erosion']
-            config['algorithm']['postprocess']['gaussianKernelSize'] = values['Gaussian']
-            config['algorithm']['postprocess']['threshold']['size'] = values['Threshold']
-            config['algorithm']['postprocess']['invertBinary'] = values['invertBinaryImage']
-            config['algorithm']['process']['colorRange']['hsv_green']['lower'][0] = int(
-                values['GreenRangeHueLow'])
-            config['algorithm']['process']['colorRange']['hsv_green']['lower'][1] = int(
-                values['GreenRangeSatLow'])
-            config['algorithm']['process']['colorRange']['hsv_green']['upper'][0] = int(
-                values['GreenRangeHueHigh'])
-            config['algorithm']['process']['colorRange']['hsv_green']['upper'][1] = int(
-                values['GreenRangeSatHigh'])
-            # Thresholding value
-            thresholdMethod = 'otsu' if values['ThreshOts'] else 'adaptive' if values['ThreshAdapt'] else 'binary'
-            config['algorithm']['postprocess']['threshold']['method'] = thresholdMethod
-            # Channel selection
-            channel = 'r' if values['RChannel'] else 'g' if values['GChannel'] else 'b' if values['BChannel'] else 'all'
-            config['algorithm']['process']['channel'] = channel
+    dpg.show_viewport()
 
-            # Update the color preview
-            updateColorPreview(
-                window, config['algorithm']['process']['colorRange'])
+    # Loop
+    while dpg.is_dearpygui_running():
+        # Get GUI values
+        alpha = dpg.get_value('camAlpha')
+        beta = dpg.get_value('camBeta')
 
-            # Resize frames if necessary
-            prevFrame = resizeFrame(prevFrame, cfgGui['maxImageHolderSize'])
-            currFrame = resizeFrame(currFrame, cfgGui['maxImageHolderSize'])
+        # Re-write the config values based on the GUI changes
+        config['algorithm']['process']['subtractRL'] = dpg.get_value(
+            'SubtractionOrder')
+        config['algorithm']['postprocess']['erosionKernelSize'] = dpg.get_value(
+            'Erosion')
+        config['algorithm']['postprocess']['gaussianKernelSize'] = dpg.get_value(
+            'Gaussian') if dpg.get_value('Gaussian') % 2 == 1 else dpg.get_value('Gaussian') + 1
+        config['algorithm']['postprocess']['threshold']['size'] = dpg.get_value(
+            'Threshold')
+        config['algorithm']['postprocess']['invertBinary'] = dpg.get_value(
+            'invertBinaryImage')
+        config['algorithm']['process']['colorRange']['hsv_green']['lower'][0] = int(
+            dpg.get_value('GreenRangeHueLow'))
+        config['algorithm']['process']['colorRange']['hsv_green']['lower'][1] = int(
+            dpg.get_value('GreenRangeSatLow'))
+        config['algorithm']['process']['colorRange']['hsv_green']['upper'][0] = int(
+            dpg.get_value('GreenRangeHueHigh'))
+        config['algorithm']['process']['colorRange']['hsv_green']['upper'][1] = int(
+            dpg.get_value('GreenRangeSatHigh'))
+        # Thresholding value
+        config['algorithm']['postprocess']['threshold']['method'] = dpg.get_value(
+            'ThreshMethod').lower()
+        # Channel selection
+        colorChannelValue = dpg.get_value('ColorChannel')
+        channel = 'r' if colorChannelValue == 'Red' else 'g' if dpg.get_value(
+            'ColorChannel') == 'Green' else 'b' if colorChannelValue == 'Blue' else 'All'
+        config['algorithm']['process']['channel'] = channel
 
-            if (cfgMode['sequentialSubtraction']):
-                # Process the frames
-                pFrame, cFrame, frameMask = processSequentialFrames(
-                    prevFrame, currFrame, True, config)
-                # Apply the mask
-                frameMaskApplied = cv.bitwise_and(
-                    cFrame, cFrame, mask=frameMask)
-                # Show the setup-specific frames
-                pFrameVis = cv.imencode(".png", prevFrame)[1].tobytes()
-                cFrameVis = cv.imencode(".png", currFrame)[1].tobytes()
-                window['FramesLeft'].update(data=pFrameVis)
-                window['FramesRight'].update(data=cFrameVis)
-            else:
-                # Keep the original frame
-                cFrameRGB = np.copy(currFrame)
-                # Process the frames
-                cFrame, frameMask = processSingleFrame(
-                    currFrame, True, config)
-                frameMaskApplied = cv.bitwise_and(
-                    cFrame, cFrame, mask=frameMask)
-                # Show the setup-specific frames
-                cFrameVis = cv.imencode(".png", cFrameRGB)[1].tobytes()
-                window['FramesMain'].update(data=cFrameVis)
+        # Retrieve frames
+        ret, currFrame = cap.read()
 
-            # Show the common frames
-            maskVis = cv.imencode(".png", frameMask)[1].tobytes()
-            maskAppliedVis = cv.imencode(".png", frameMaskApplied)[1].tobytes()
-            window['FramesMask'].update(data=maskVis)
-            window['FramesMaskApplied'].update(data=maskAppliedVis)
+        # Break out of the loop if the video is finished
+        if not ret:
+            break
 
-            # ArUco marker detection
-            frameMarkers = arucoMarkerDetector(
-                frameMask, None, None, cfgMarker['detection']['dictionary'],
-                cfgMarker['structure']['size'])
-            frameMarkersVis = cv.imencode(
-                ".png", frameMarkers)[1].tobytes()
-            window['FramesMarker'].update(data=frameMarkersVis)
+        # Resize frames if necessary
+        currFrame = resizeFrame(currFrame, cfgGui['maxImageHolderSize'])
 
-            # Record the frame(s)
-            if event == 'Record':
-                frameMarkers = cv.cvtColor(frameMarkers, cv.COLOR_GRAY2BGR)
-                imageList = [prevFrame, currFrame, frameMarkers] if (
-                    cfgMode['sequentialSubtraction']) else [currFrame, frameMarkers]
-                concatedImage = imageConcatHorizontal(imageList, 1800)
-                frameSave(concatedImage, cfgMode['runner'])
+        # Rotate the frame 180 degrees (if necessary)
+        if cfgOffline['video']['rotate']:
+            currFrame = cv.rotate(currFrame, cv.ROTATE_180)
 
-            # Save the previous frame
+        # Only the first time, copy the current frame to the previous frame
+        if prevFrame is None:
             prevFrame = np.copy(currFrame)
 
-    finally:
-        # Stop the pipeline and close the windows
-        cv.destroyAllWindows()
-        print(
-            f'Framework stopped! [Offline Video Captured by Single Vision Setup - {setupVariant}]')
+        # Update the color preview
+        # updateColorPreview(
+        #     window, config['algorithm']['process']['colorRange'])
+
+        # Change brightness
+        prevFrame = cv.convertScaleAbs(prevFrame, alpha=alpha, beta=beta)
+        currFrame = cv.convertScaleAbs(currFrame, alpha=alpha, beta=beta)
+
+        if (isSequential):
+            # Process the frames
+            prevFrame, currFrame, frameMask = processSequentialFrames(
+                prevFrame, currFrame, True, config)
+            # Apply the mask
+            frameMaskApplied = cv.bitwise_and(
+                currFrame, currFrame, mask=frameMask)
+        else:
+            # Process the frames
+            currFrame, frameMask = processSingleFrame(
+                currFrame, True, config)
+            frameMaskApplied = cv.bitwise_and(
+                currFrame, currFrame, mask=frameMask)
+
+        # Convert to RGB
+        frameMask = cv.cvtColor(frameMask, cv.COLOR_GRAY2BGR)
+
+        # Camera parameters
+        distCoeffs = distCoeffs_RealSense
+        cameraMatrix = cameraMatrix_RealSense
+
+        # ArUco marker detection
+        frameMarkers = arucoMarkerDetector(
+            frameMask, cameraMatrix, distCoeffs, cfgMarker['detection']['dictionary'],
+            cfgMarker['structure']['size'])
+
+        # Update the textures
+        onImageViewTabChange({
+            'left': prevFrame,
+            'right': currFrame,
+            'main': currFrame,
+            'mask': frameMask,
+            'maskApplied': frameMaskApplied,
+            'marker': frameMarkers
+        })
+
+        # Record the frame(s)
+        if dpg.get_value("RecordFlag"):
+            imageList = [prevFrame, currFrame, frameMarkers] if (
+                isSequential) else [currFrame, frameMarkers]
+            concatedImage = imageConcatHorizontal(imageList, 1800)
+            frameSave(concatedImage, cfgMode['runner'])
+            dpg.set_value("RecordFlag", False)
+
+        # You can manually stop by using stop_dearpygui()
+        dpg.render_dearpygui_frame()
+
+        # Save the previous frame
+        prevFrame = np.copy(currFrame)
+
+    print(
+        f'Framework stopped! [Offline Video Captured by Single Vision Setup - {setupVariant}]')
+    cv.destroyAllWindows()
+    dpg.destroy_context()
